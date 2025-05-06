15038966d93b52abe303e52f5517081efedc6d59
Sharon Chung <20680490+chunggainchi@users.noreply.github.com>
2025-05-06 14:40:04 +0200

    Updated GA setup to use thrid parties setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
-import Script from 'next/script';
 import Layout from "@/components/Layout";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const GA_TRACKING_ID = "G-CS5K6LLQFH";
 
@@ -16,28 +16,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <head>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-        />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
+        {/* Old Script implementation removed */}
       </head>
       <body>
         <Layout>{children}</Layout>
       </body>
+      <GoogleAnalytics gaId={GA_TRACKING_ID} />
     </html>
   );
 }
